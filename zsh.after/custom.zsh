591d551240ef4d02e0100fe054aa3a728df1c31b
Forest Carlisle <forestcarlisle@gmail.com>
2013-11-05 15:26:21 -0800

    Load boxen before rbenv so rbenv uses the boxen installed one.

--- zsh.after/custom.zsh
+++ zsh.after/custom.zsh
@@ -7,10 +7,11 @@ export RUBY_GC_MALLOC_LIMIT=1000000000
 export RUBY_FREE_MIN=500000
 export RUBY_HEAP_MIN_SLOTS=40000
 
+# boxen
+export BOXEN_SRC_DIR=$HOME/code
+[ -f /opt/boxen/env.sh ] && source /opt/boxen/env.sh
+
 # To enable shims and autocompletion add to your profile:
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
-# boxen
-#export BOXEN_SRC_DIR=$HOME/code
-#[ -f /opt/boxen/env.sh ] && source /opt/boxen/env.sh
  
