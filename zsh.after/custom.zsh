c365b7eaa1fc5b3ba84c2304bdb686da8439365f
Forest Carlisle <forestcarlisle@gmail.com>
2014-02-24 17:01:38 -0800

    load direnv

--- zsh.after/custom.zsh
+++ zsh.after/custom.zsh
@@ -7,11 +7,14 @@ export RUBY_GC_MALLOC_LIMIT=1000000000
 export RUBY_FREE_MIN=500000
 export RUBY_HEAP_MIN_SLOTS=40000
 
+export RUBYLIB=/Applications/RubyMine.app/rb/testing/patch/common:/Applications/RubyMine.app/rb/testing/patch/bdd
+
+# load direnv
+if which direnv > /dev/null; then eval "$(direnv hook zsh)"; fi
+
 # boxen
 export BOXEN_SRC_DIR=$HOME/code
 [ -f /opt/boxen/env.sh ] && source /opt/boxen/env.sh
 
 # To enable shims and autocompletion add to your profile:
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
- 
