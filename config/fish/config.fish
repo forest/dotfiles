050fd8a99f86715e859342c8268979f94a92c288
Forest Carlisle <forestcarlisle@gmail.com>
2021-09-01 17:30:41 -0700

    fuzzy finder

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -29,9 +29,11 @@ export EDITOR="vim"
 export VISUAL="code"
 
 set -g fish_user_paths /usr/local/opt/gettext/bin $fish_user_paths
-export ERL_AFLAGS="-kernel shell_history enabled"
-export FZF_DEFAULT_OPTS='--height 30%'
 
+
+export FZF_DEFAULT_OPTS='--height 40% --border'
+
+export ERL_AFLAGS="-kernel shell_history enabled"
 # build Erlang with docs
 export KERL_BUILD_DOCS="yes"
 
