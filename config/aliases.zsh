772fa8f5dc87480c730a86bc9f55c8c226619663
Forest Carlisle <7829+forest@users.noreply.github.com>
2023-09-22 11:15:14 -0700

    up

--- config/aliases.zsh
+++ config/aliases.zsh
@@ -55,5 +55,5 @@ alias pgst='pg_ctl -D $(brew --prefix)/var/postgres start'
 alias pgsp='pg_ctl -D $(brew --prefix)/var/postgres stop -s -m fast'
 
 # list (ls)
-alias la='exa --long --header --git --no-user'
-alias lt='exa --long --header --git --no-user --tree --git-ignore'
+alias la='eza --long --header --git --no-user'
+alias lt='eza --long --header --git --no-user --tree --git-ignore'
